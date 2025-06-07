63c21ce79df9137662b6832e9e11fcc18f42a89b
Danylo-D87 <quaestor88@gmail.com>
2025-06-07 12:30:48 +0300

    sol

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,61 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import func
+from typing import Optional
+from database import get_db
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
+from database.models import MovieModel
 
-from database import get_db, MovieModel
+
+router =APIRouter()
 
 
-router = APIRouter()
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+    page: int = Query(1, ge=1),
+    per_page: int = Query(10, ge=1, le=20),
+    db: AsyncSession = Depends(get_db)
+):
+    # Загальна кількість фільмів
+    total_items_res = await db.execute(select(func.count(MovieModel.id)))
+    total_items = total_items_res.scalar_one()
+    if total_items == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
 
-# Write your code here
+    total_pages = (total_items + per_page - 1) // per_page
+
+    if page > total_pages and total_pages != 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    offset = (page - 1) * per_page
+
+    query = await db.execute(
+        select(MovieModel).offset(offset).limit(per_page)
+    )
+    movies = query.scalars().all()
+
+    base_path = "/movies/"
+    prev_page = f"{base_path}?page={page-1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_path}?page={page+1}&per_page={per_page}" if page < total_pages else None
+
+    movies_pydantic = [MovieDetailResponseSchema.model_validate(movie) for movie in movies]
+    return MovieListResponseSchema(
+        movies=movies_pydantic,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items
+    )
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie_by_id(
+    movie_id: int,
+    db: AsyncSession = Depends(get_db)
+):
+    query = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = query.scalar_one_or_none()
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return movie
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,30 @@
-# Write your code here
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import date
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: int
+    revenue: float
+    country: str
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[str] = None
+    next_page: Optional[str] = None
+    total_pages: int
+    total_items: int
